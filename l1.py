379c8486c2aa4051ae3b6cce95721fa46f855937
dongxingtao <dxt@slofzx.com>
2024-02-01 14:33:04 +0800

    a

--- l1.py
+++ l1.py
@@ -1,1 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
+from fastapi_cdn_host import monkey_patch_for_docs_ui
+
+app = FastAPI()
+monkey_patch_for_docs_ui(app)
+
+@app.get("/get")
+def get_test():
+    return {"method":"get method"}
+
+if __name__ =="__main__":
+    uvicorn.run(app='l1:app', port=8000 , reload=True , workers=3)
